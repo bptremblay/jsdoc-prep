e3c5bdf27707bbbbf6944cb803bdc757e801be32
Benjamin P Tremblay <benjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.com>
2014-07-16 20:06:20 -0400

    Added line/linenumber. TODO: up to this point the editor was inserting TABS

--- test-source/no-module.js
+++ test-source/no-module.js
@@ -5,42 +5,42 @@
  * @copyright JPMorgan Chase & Co. All rights reserved.
  */
 function Fronum() {
-	this.chewBakka = function() {
-		return function DonutView() {
-			return Fronum.prototype;
-		}
-	};
+    this.chewBakka = function() {
+        return function DonutView() {
+            return Fronum.prototype;
+        }
+    };
 
-	/*
-	 * This function is stupid.
-	 */
-	this["stupidFunction"] = function() {
-		var output = 100;
-		output++;
-		return output;
-	};
+    /*
+     * This function is stupid.
+     */
+    this["stupidFunction"] = function() {
+        var output = 100;
+        output++;
+        return output;
+    };
 
-	/**
-	 * @private
-	 * @param a
-	 * @param b
-	 * @param c
-	 * @return <String>
-	 */
-	var privateFunction = function(a, b, c) {
-		return 'hodag zero';
-	};
+    /**
+     * @private
+     * @param a
+     * @param b
+     * @param c
+     * @return <String>
+     */
+    var privateFunction = function(a, b, c) {
+        return 'hodag zero';
+    };
 }
 /**
  * 
  */
 Fronum.prototype.worldPuzzle = function(toothache, tomato) {
-	var x = 0;
-	return x - 100;
+    var x = 0;
+    return x - 100;
 };
 
 function SecondClass() {
 }
 SecondClass.prototype.fixTheWorld = function(toothache, tomato) {
-	return false;
+    return false;
 };
