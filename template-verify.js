bd46a933deb13fdfc0151c3ba1a45048087e07c3
Benjamin P Tremblay <benjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.com>
2014-10-21 07:26:11 -0400

    Fixed formatting, etc.

--- template-verify.js
+++ template-verify.js
@@ -7,6 +7,7 @@ var sys = require('sys');
 var FILE_ENCODING = 'utf8';
 var SCAN_PATH = '';
 var stat;
+
 /**
  * Filter files.
  *
@@ -15,22 +16,23 @@ var stat;
  * @return {Array<Object>} A filtered list of file objects.
  */
 function filterFiles(files, excludes) {
-	var globOpts = {
-		matchBase : true,
-		dot : true
-	};
-	excludes = excludes.map(function(val) {
-		return _minimatch.makeRe(val, globOpts);
-	});
-	files = files.map(function(filePath) {
-		return _path.normalize(filePath).replace(/\\/g, '/');
-	});
-	return files.filter(function(filePath) {
-		return !excludes.some(function(glob) {
-			return glob.test(filePath);
-		});
-	});
+    var globOpts = {
+        matchBase: true,
+        dot: true
+    };
+    excludes = excludes.map(function(val) {
+        return _minimatch.makeRe(val, globOpts);
+    });
+    files = files.map(function(filePath) {
+        return _path.normalize(filePath).replace(/\\/g, '/');
+    });
+    return files.filter(function(filePath) {
+        return !excludes.some(function(glob) {
+            return glob.test(filePath);
+        });
+    });
 }
+
 /**
  * Normalize name.
  *
@@ -38,8 +40,9 @@ function filterFiles(files, excludes) {
  * @return {String}
  */
 function normalizeName(input) {
-	return input.split('_').join('-');
+    return input.split('_').join('-');
 }
+
 /**
  * Read file.
  *
@@ -47,54 +50,55 @@ function normalizeName(input) {
  * @return {String}
  */
 function readFile(filePathName) {
-	var FILE_ENCODING = 'utf8';
-	filePathName = _path.normalize(filePathName);
-	var source = '';
-	try {
-		source = _fs.readFileSync(filePathName, FILE_ENCODING);
-	} catch (er) {
-	}
-	return source;
+    var FILE_ENCODING = 'utf8';
+    filePathName = _path.normalize(filePathName);
+    var source = '';
+    try {
+        source = _fs.readFileSync(filePathName, FILE_ENCODING);
+    } catch (er) {}
+    return source;
 }
+
 /**
  * Run.
  * @param options
  */
 function run(options) {
-	var then = 0;
-	SCAN_PATH = options.scanPath;
-	console.log('Starting Ractive template check: ' + SCAN_PATH);
-	var files = _wrench.readdirSyncRecursive(SCAN_PATH);
-	files = filterFiles(files, [ '.*', '.DS_Store', '{**/,}.git{/**,}',
-			'node_modules/**', 'bower_components/**', 'build/**', 'css/**',
-			'img/tmp/**', 'build.js', 'gui.html', 'package.json', 'README.md',
-			'update.sh', 'yui_sdk/**', 'infrastructure/**', '{**/,}.html' ]);
-	var good = 0;
-	var bad = 0;
-	var total = 0;
-	files.forEach(function(path) {
-		path = _path.normalize(SCAN_PATH + '/' + path);
-		stat = _fs.statSync(path);
-		if (stat.isFile() && path.indexOf('.html') !== -1) {
-			if (_path.extname(path) === '.html') {
-				total++;
-				try {
-					var source = readFile(path);
-					var parseResult = ractive.parse(source);
-					good++;
-				} catch (parseError) {
-					parseError.fileName = path;
-					bad++;
-					console.warn(parseError);
-				}
-			}
-		}
-	});
-	console.log('Ractive template check of ' + total
-			+ ' template files is complete. Found ' + bad
-			+ ' templates with errors.');
-	then = new Date().getTime();
+    var then = 0;
+    SCAN_PATH = options.scanPath;
+    console.log('Starting Ractive template check: ' + SCAN_PATH);
+    var files = _wrench.readdirSyncRecursive(SCAN_PATH);
+    files = filterFiles(files, ['.*', '.DS_Store', '{**/,}.git{/**,}',
+        'node_modules/**', 'bower_components/**', 'build/**', 'css/**',
+        'img/tmp/**', 'build.js', 'gui.html', 'package.json', 'README.md',
+        'update.sh', 'yui_sdk/**', 'infrastructure/**', '{**/,}.html'
+    ]);
+    var good = 0;
+    var bad = 0;
+    var total = 0;
+    files.forEach(function(path) {
+        path = _path.normalize(SCAN_PATH + '/' + path);
+        stat = _fs.statSync(path);
+        if (stat.isFile() && path.indexOf('.html') !== -1) {
+            if (_path.extname(path) === '.html') {
+                total++;
+                try {
+                    var source = readFile(path);
+                    var parseResult = ractive.parse(source);
+                    good++;
+                } catch (parseError) {
+                    parseError.fileName = path;
+                    bad++;
+                    console.warn(parseError);
+                }
+            }
+        }
+    });
+    console.log('Ractive template check of ' + total + ' template files is complete. Found ' + bad + ' templates with errors.');
+    then = new Date().getTime();
 }
+
 run({
-	scanPath : 'src/dashboard/template'
+    // Supply the path to scan here.
+    scanPath: 'src/dashboard/template'
 });
