4f64495e2a274e560e234f8d9eb80e3bf34f74f5
Benjamin P Tremblay <benjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.com>
2014-10-22 14:21:20 -0400

    asynchronous version, with sample media

--- template-verify.js
+++ template-verify.js
@@ -19,8 +19,8 @@ var stat;
  */
 function filterFiles(files, excludes) {
     var globOpts = {
-        matchBase: true,
-        dot: true
+            matchBase: true,
+            dot: true
     };
     excludes = excludes.map(function (val) {
         return _minimatch.makeRe(val, globOpts);
@@ -73,6 +73,23 @@ function run(options) {
     SCAN_PATH = options.scanPath;
     var templateType = options.templateType;
     var fileTypes = [];
+    var good = 0;
+    var bad = 0;
+    var total = 0;
+    var fileLength = 0;
+    var fileCount = 0;
+    
+    var whenDone = function(){
+        if (fileLength === fileCount){
+           // console.log(total + ',' + good + ',' + bad);
+            if (total === (good + bad)){
+                console.log(templateType + ' template check of ' + total + ' template files is complete. Found ' + bad + ' templates with errors.');
+                then = new Date().getTime();
+            }
+        }
+        
+    };
+    
     if (templateType == null) {
         templateType = 'RACTIVE';
     }
@@ -86,74 +103,80 @@ function run(options) {
     console.log('Starting ' + templateType + ' template check: ' + SCAN_PATH);
     var files = _wrench.readdirSyncRecursive(SCAN_PATH);
     files = filterFiles(files, ['.*', '.DS_Store', '{**/,}.git{/**,}',
-        'node_modules/**', 'bower_components/**', 'build/**', 'css/**',
-        'img/tmp/**', 'build.js', 'gui.html', 'package.json', 'README.md',
-        'update.sh', 'yui_sdk/**', 'infrastructure/**', '{**/,}.html'
-    ]);
-    var good = 0;
-    var bad = 0;
-    var total = 0;
+                                'node_modules/**', 'bower_components/**', 'build/**', 'css/**',
+                                'img/tmp/**', 'build.js', 'gui.html', 'package.json', 'README.md',
+                                'update.sh', 'yui_sdk/**', 'infrastructure/**', '{**/,}.html'
+                                ]);
+    fileLength = files.length;
     files
-        .forEach(function (path) {
-            path = _path.normalize(SCAN_PATH + '/' + path);
-            stat = _fs.statSync(path);
-            if (stat.isFile() && (path.indexOf('.html') !== -1 || path
-                    .indexOf('.hbs') !== -1) || path.indexOf('.oft') !== -1 || path.indexOf('.less') !== -1) {
-                if (_path.extname(path) === '.html' || _path.extname(path) === '.hbs' || _path.extname(path) === '.oft' || _path.extname(path) === '.less') {
-                    total++;
-                    var fileName = _path.basename(path);
-                    var pathName = _path.dirname(path);
-                    var source = readFile(path);
-                    var parseResult = null;
-                    if (templateType.toUpperCase() === 'RACTIVE') {
-                        try {
-                            parseResult = ractive.parse(source);
-                            good++;
-                        } catch (parseError) {
-                            parseError.fileName = path;
-                            bad++;
-                            console.warn(parseError);
-                        }
-                    } else if (templateType.toUpperCase() === 'HANDLEBARS') {
-                        // FIXME: use HBS compiler
-                        try {
-                            parseResult = handlebars.compile(source);
-                            good++;
-                        } catch (parseError) {
-                            parseError.fileName = path;
-                            bad++;
-                            console.warn(parseError);
-                        }
-                    } else if (templateType.toUpperCase() === 'LESS') {
-                        // FIXME: use LESS compiler
-                        var opts = {
+    .forEach(function (path) {
+        path = _path.normalize(SCAN_PATH + '/' + path);
+        stat = _fs.statSync(path);
+        fileCount++;
+        if (stat.isFile() && (path.indexOf('.html') !== -1 || path
+                .indexOf('.hbs') !== -1) || path.indexOf('.oft') !== -1 || path.indexOf('.less') !== -1) {
+            if (_path.extname(path) === '.html' || _path.extname(path) === '.hbs' || _path.extname(path) === '.oft' || _path.extname(path) === '.less') {
+                total++;
+                var fileName = _path.basename(path);
+                var pathName = _path.dirname(path);
+                var source = readFile(path);
+                var parseResult = null;
+                if (templateType.toUpperCase() === 'RACTIVE') {
+                    try {
+                        parseResult = ractive.parse(source);
+                        good++;
+                    } catch (parseError) {
+                        parseError.fileName = path;
+                        bad++;
+                        console.warn(parseError);
+                    }
+                    whenDone();
+                } else if (templateType.toUpperCase() === 'HANDLEBARS') {
+                    // FIXME: use HBS compiler
+                    try {
+                        parseResult = handlebars.compile(source);
+                        good++;
+                    } catch (parseError) {
+                        parseError.fileName = path;
+                        bad++;
+                        console.warn(parseError);
+                    }
+                    whenDone();
+                } else if (templateType.toUpperCase() === 'LESS') {
+                    // FIXME: use LESS compiler
+                    var opts = {
                             filename: fileName,
                             paths: [pathName]
-                        };
-                        try {
-                            less.render(source, opts, function (parseError,
+                    };
+                    try {
+                        less.render(source, opts, function (parseError,
                                 css) {
-                                if (parseError != null) {
-                                    parseError.fileName = path;
-                                    bad++;
-                                    console.warn(parseError);
-                                } else {
-                                    // audit output here:
-                                    // console.log(css);
-                                    good++;
-                                }
-                            });
-                        } catch (parseError) {
-                            parseError.fileName = path;
-                            bad++;
-                            console.warn(parseError);
-                        }
+                            if (parseError != null) {
+                                parseError.fileName = path;
+                                bad++;
+                                console.warn(parseError);
+                            } else {
+                                // audit output here:
+                                // console.log(css);
+                                good++;
+                            }
+                            whenDone();
+                        });
+                    } catch (parseError) {
+                        parseError.fileName = path;
+                        bad++;
+                        console.warn(parseError);
+                        whenDone();
                     }
                 }
             }
-        });
-    console.log(templateType + ' template check of ' + total + ' template files is complete. Found ' + bad + ' templates with errors.');
-    then = new Date().getTime();
+
+        }
+
+    });
+
+   
+
 }
 
 run({
