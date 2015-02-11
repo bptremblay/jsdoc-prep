4b3f4d8495613b2135c451d5cabd70fcf7df0502
Benjamin P Tremblay <benjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.combenjamin.p.tremblay@jpmchase.com>
2015-02-11 13:49:08 -0500

    make ui asynchronous

--- interactive/js/index.js
+++ interactive/js/index.js
@@ -1,6 +1,34 @@
 
 function process(){
-    var input = document.getElementById('input').value;
-    document.getElementById('output').value = input;
-    document.getElementById('results').innerHTML = '';
+    $.getJSON('json/data.json', function(json){
+        processSource(json).then(function(json){
+            generateDoc(json).then(
+                    function(json){
+                        update(json);
+                    }
+            )
+        });
+    });
+}
+
+function update(json){
+    $('#input').html(json.input);
+    $('#output').html(json.output);
+    $('#results').html(json.results);
+}
+
+function processSource(json){
+    var when = new Promise(function(resolve, reject){
+        json.output = json.input.trim().toLowerCase();
+        resolve(json);
+    });
+    return when;
+}
+
+function generateDoc(json){
+    var when = new Promise(function(resolve, reject){
+        json.results ='<h1>dummy</h1>';
+        resolve(json);
+    });
+    return when;
 }
