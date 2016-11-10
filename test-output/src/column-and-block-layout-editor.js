5bb4cb36afd917e368b27716bb7e8c5c1be57998
Ben Tremblay <btremblay@constantcontact.com>
2016-11-10 13:05:16 -0500

    remove dead files

--- test-output/src/column-and-block-layout-editor.js
+++
@@ -1,8 +0,0 @@
-define(['column-and-block-layout-editor-path/index'],
-  /**
-   * @exports src/column-and-block-layout-editor
-   * @requires column-and-block-layout-editor-path/index
-   */
-  function (ColumnAndBlockLayoutEditor) {
-    return ColumnAndBlockLayoutEditor;
-  });
